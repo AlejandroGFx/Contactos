cec08a0606195c5ebae2acd91b09a73179a0724b
Alejandro <alejandroogutierrez06@gmail.com>
2026-01-06 15:55:39 +0100

    hola

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
 <?php
+require_once 'lib/db/db.php';
+
+if (isset($pdo)){
+    echo "¡Conexión exitosa a la base de datos MySQL!";
+}
--- lib/db/db.php
+++ lib/db/db.php
@@ -1,2 +1,27 @@
 <?php
 require_once  __DIR__ . '/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+}catch (\PDOException $e){
+    die("Error de conexion: " . $e->getMessage());
+}
